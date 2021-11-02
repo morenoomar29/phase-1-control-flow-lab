ecca5dcaeaf68a5ddadd10042f64ee8a5f813dff
Omar <morenoomar29@yahoo.com>
2021-11-02 15:24:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+    if (distance<=400) {
+      return 'This one is on me!'
+  } else if (distance>2500){
+      return 'No can do.'
+    //return 'No can do.'
+  } else if (distance>2000)
+       return 'I will gladly take your thirty bucks.'
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+/*scuberGreetingForFeet(199)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
+*/
+
+
+function ternaryCheckCity(city){
+  {return city === "NYC" ? 'Ok, sounds good.' : 'No go.' }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case'generous':
+      return 'Thank you so much.'
+    case'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
